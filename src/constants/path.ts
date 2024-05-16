45905d72bb4f11092d93d2c11c98d4d839a84f96
hanyoung <phanyoung@naver.com>
2024-05-16 18:31:50 +0900

    chore: 배포를 위한 빌드

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,4 +1,7 @@
+const IS_PRODUCTION = true;
+const BASE_URL = IS_PRODUCTION ? "/react-shopping-cart/dist" : "/dist/index.html";
+
 export const PATH = {
-  cart: "/",
-  orderSummary: "/order-summary",
+  cart: BASE_URL + "/",
+  orderSummary: BASE_URL + "/order-summary",
 };

618cfdac0bc86aedcfee373de7f502ebf8979564
hanyoung <phanyoung@naver.com>
2024-05-20 22:11:25 +0900

    배포를 위한 재빌드

--- src/constants/routePath.ts
+++ src/constants/routePath.ts
@@ -1,4 +1,7 @@
+const IS_PRODUCTION = true;
+const BASE_URL = IS_PRODUCTION ? "/react-shopping-cart/dist" : "/dist/index.html";
+
 export const ROUTE_PATH = {
-  cart: "/",
-  checkout: "/checkout",
+  cart: BASE_URL + "/",
+  checkout: BASE_URL + "/checkout",
 };
